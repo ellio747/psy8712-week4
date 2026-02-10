849525e7af2b0b7c2063ebe0773ebb3d846bd487
Ben Elliott <ellio747@umn.edu>
2026-02-10 15:25:02 -0600

    completed L2-L8

--- R/week4.R
+++ R/week4.R
@@ -1,2 +1,8 @@
 # Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+library(tidyverse)
+
+# Data Import
+import_tbl <- read_delim("../data/week4.dat", delim = "-", col_names = c("casenum", "parnum", "stimver", "datadate", "qs"))
+glimpse(import_tbl) # I think glimpse is correct; it comes from dplyr within tidyverse
+wide_tbl <- separate(import_tbl, col = qs, into = paste0("q", 1:5))
