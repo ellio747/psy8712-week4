a404d69e143a5303ce912eb9c7949b4ffa88d943
Ben Elliott <ellio747@umn.edu>
2026-02-13 07:28:54 -0600

    lines 7-13 completed

--- R/week4.R
+++ R/week4.R
@@ -4,5 +4,10 @@ library(tidyverse)
 
 # Data Import
 import_tbl <- read_delim("../data/week4.dat", delim = "-", col_names = c("casenum", "parnum", "stimver", "datadate", "qs"))
-glimpse(import_tbl) # I think glimpse is correct; it comes from dplyr within tidyverse
-wide_tbl <- separate(import_tbl, col = qs, into = paste0("q", 1:5))
+glimpse(import_tbl) 
+wide_tbl <- import_tbl %>% separate(., col = qs, into = paste0("q", 1:5))
+wide_tbl[, paste0("q", 1:5)] <- sapply(wide_tbl[, paste0("q", 1:5)], as.integer)
+wide_tbl <- wide_tbl %>% mutate(datadate = mdy_hms(datadate))
+wide_tbl <- wide_tbl %>% mutate(across(paste0("q", 1:5), ~ ifelse(. == 0, NA, .)))
+wide_tbl <- wide_tbl %>% drop_na(q2)
+long_tbl <- wide_tbl %>% pivot_longer(-c(casenum, parnum, stimver, datadate), names_to = "question", values_to = "response")
