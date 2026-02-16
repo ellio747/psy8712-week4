457aae621b8e58c52c0c947450e8b376f0d2e5f3
Ben Elliott <ellio747@umn.edu>
2026-02-16 11:52:34 -0600

    updated comment line 11 & 12 and removed note in comments for line 9

--- R/week4.R
+++ R/week4.R
@@ -6,8 +6,8 @@ library(tidyverse)
 import_tbl <- read_delim("../data/week4.dat", delim = "-", col_names = c("casenum", "parnum", "stimver", "datadate", "qs"))
 glimpse(import_tbl) 
 wide_tbl <- separate(import_tbl, col = qs, into = c("q1", "q2","q3","q4", "q5"))
-wide_tbl[, c("q1", "q2","q3","q4", "q5")] <- sapply(wide_tbl[, c("q1", "q2","q3","q4", "q5")], as.integer) #Note: one response to q4 was a 6. Maybe a response issue.
+wide_tbl[, c("q1", "q2","q3","q4", "q5")] <- sapply(wide_tbl[, c("q1", "q2","q3","q4", "q5")], as.integer) # one response to q4 was a 6. Maybe a response issue.
 wide_tbl <- mutate(wide_tbl, datadate = mdy_hms(datadate)) # converts to POSIXct format; verified in class(wide_tbl$datadate)
-wide_tbl <- mutate(wide_tbl, across(q1:q5, ~ na_if(., 0))) # 18 NA in place of 0
-wide_tbl <- drop_na(wide_tbl, q2) # will remove 9 subjects with NA for q2; sum(is.na(wide_tbl[, 5:9]))
+wide_tbl <- mutate(wide_tbl, across(q1:q5, ~ na_if(., 0))) # 18 NA in place of 0; sum(is.na(wide_tbl[, 5:9]))
+wide_tbl <- drop_na(wide_tbl, q2) # will remove 9 subjects with NA for q2
 long_tbl <- pivot_longer(wide_tbl, cols = q1:q5, names_to = "question", values_to = "response")
